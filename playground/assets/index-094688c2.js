e8aa29503c543ac59a9d4148f417001e8cfb261d
nikolaynau <nikolaynau@users.noreply.github.com>
2023-05-08 11:11:32 +0000

    deploy: 0f5310e91bd4331a54df12a39565e5b3fb1959d9

---
+++ playground/assets/index-094688c2.js
@@ -0,0 +1,1 @@
+import{l}from"./index-b3c8a946.js";import{u as c}from"./index-6431710d.js";import{s as f,m as u}from"./index-093e58bd.js";function w(n={}){const{factory:t,dispose:r=!0,...e}=n,o=f(null);function a(){const s=t?t(e):new l.Control.Scale(e);o.value=u(s)}return c(o,{dispose:r}),a(),o}export{w as u};
---
+++ playground/assets/index-1ca43c12.js
@@ -0,0 +1,1 @@
+import{e as t,f as a}from"./index-b3c8a946.js";import{y as r}from"./index-093e58bd.js";function f(e,...o){return r(()=>t(...o).value?a(e):void 0)}export{f as u};
