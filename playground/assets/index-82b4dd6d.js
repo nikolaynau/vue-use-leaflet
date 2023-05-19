10bc2e42d1fe5005d1960fc839aad276460e9b37
nikolaynau <nikolaynau@users.noreply.github.com>
2023-05-19 15:07:52 +0000

    deploy: 5ea9fc0c83c1de5ff89c2346ff0bf01e90072a25

--- playground/assets/index-82b4dd6d.js
+++
@@ -1,1 +0,0 @@
-import{l as o}from"./index-1913f7a9.js";import{u as p}from"./index-3b950fc2.js";function y(t,e={}){const{factory:n=a,defOptions:r=o.Polygon.prototype.options,...s}=e,l=p(t,{factory:n,defOptions:r,...s});function a(f,i){return new o.Polygon(f,i)}return l}export{y as u};
---
+++ playground/assets/index-b36bf6bd.js
@@ -0,0 +1,1 @@
+import{l as o}from"./index-a672d8d6.js";import{u as p}from"./index-2d6f2f1c.js";function y(t,e={}){const{factory:n=a,defOptions:r=o.Polygon.prototype.options,...s}=e,l=p(t,{factory:n,defOptions:r,...s});function a(f,i){return new o.Polygon(f,i)}return l}export{y as u};
